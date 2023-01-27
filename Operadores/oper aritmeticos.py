09ad6e44db97939d563ef1c8951081f6c94dd95e
AngelD <angelduarteing@gmail.com>
2023-01-27 11:36:52 -0500

    wew

---
+++ Operadores/oper aritmeticos.py
@@ -0,0 +1,18 @@
+# Suma y Resta ( + y - )
+suma = 3 + 5
+resta = 8 - 3
+
+# Multiplicación y División ( * y / )
+multi = 8 * 6
+divi = 20 / 5 # devuelve siempre un dato float
+
+# Potenciacion Exponente ( ** )
+exponente = 2 ** 3
+
+# División baja ( // ) Devuelve entero y redondea hacia abajo
+div_baja = 8 // 3
+
+# Resto o Modulo ( % ) multiplica el segundo numero n veces hata acercarce al primero
+# lo que quede faltando es el modulo
+Modulo = 23 % 7 
+print(Modulo)
