058b802910075e030107f0cc986c6c38fc025582
AngelD <angelduarteing@gmail.com>
2023-01-26 20:07:05 -0500

    Estudiando python

---
+++ Variables/Variables.py
@@ -0,0 +1,18 @@
+a = 10
+b = 6
+c = a * b
+e = a * b
+d = "AngelD"
+e += 1 
+print(c)
+print(d)
+print(e)
+
+# Concatenar 
+nombre = "lucas"
+apellido = "perez"
+edad= 34
+name_completo = "su nombre es "+ nombre +" "+ apellido
+name_edad = nombre + " " + apellido + f" y su edad es {edad}"
+print(name_completo)
+print(name_edad)
---
+++ tipos de datos/Datos simples.py
@@ -0,0 +1,24 @@
+"string"
+'string'
+""" string triple comilla doble
+    puedo escribir en mas de una linea"""
+
+''' string
+    tambien puedo escribir en varias lineas'''
+    
+#int = numero entero
+40
+20
+
+#float = numero con decimal
+40.2
+20.56
+
+#bool = dos posibilidades ej: verdadero o falso
+True 
+False
+
+str
+int
+float
+bool
