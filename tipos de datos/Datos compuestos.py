dda2d3b1c52f3bcf2ed61899491399501c503626
AngelD <angelduarteing@gmail.com>
2023-01-27 10:11:47 -0500

    aaa

---
+++ tipos de datos/Datos compuestos.py
@@ -0,0 +1,12 @@
+# list: lista de varios elementos se puede modificar (se usa corchete)
+lista = ["Angel","Duarte",True,34]
+lista[0] = "Eduardo"
+print(lista)
+
+# tupla: lista de varios elementos pero no se puede modificar (se usa parentesis)
+tupla = ("Angel","Duarte",True,34)
+# tupla[0] = "Eduardo" (sale error debido a que la tupla es inmodificable)
+print(tupla) 
+
+# set: crear conjunto, no tiene orden fijo y tampoco se puede modificar (se usa llaves)
+conjunto = {"Angel","Duarte",True,34}
