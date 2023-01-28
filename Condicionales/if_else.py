b5e27ebef3747702f4b01b84168991f4b5fc139d
AngelD <angelduarteing@gmail.com>
2023-01-27 19:03:12 -0500

    mas

---
+++ Condicionales/if_else.py
@@ -0,0 +1,43 @@
+#ejemplo 1
+edad_solicitada = 30
+edad_usuario = 25
+
+if edad_usuario >= edad_solicitada:
+    print ("puedes pasar")
+else:
+    print ("no puedes pasar pelotudo")
+
+#ejemplo 2
+ingreso_mensual = 4000
+
+if ingreso_mensual >= 3000:
+    print("el man tiene dinero")
+
+elif ingreso_mensual >= 2000:
+    print("el man vive comodo en latinoamerica")
+
+elif ingreso_mensual >= 1000:
+    print("vive comodo en colombia")
+
+elif ingreso_mensual >= 500:
+    print("vive comodo en venezuela")
+    
+else:
+    print("el man es pobre, repaila")
+    
+#ejemplo 3
+nota_1 = 9
+nota_2 = 7
+nota_3 = 5
+nota_4 = 3
+nota_alumno = 14
+
+if nota_alumno >= nota_1 and nota_alumno <= 15:
+    print ("Excelente")
+
+elif nota_alumno > 15:
+    print("Eres superdotado perrazo")    
+
+else:
+    print ("Estudia mucho mas, solo queremos perfección")
+    
---
+++ Operadores/oper logicos.py
@@ -0,0 +1,7 @@
+# and ( & ) 
+# or ( | )
+# not ( not )
+
+#Usar las palabras para poner varias condiciones, puede ser dentro de un if
+#usar los simbolo para poner si es true o fales (bolean)
+
