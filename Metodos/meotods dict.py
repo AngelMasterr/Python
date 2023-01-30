71b1ed47198bf6618fe47e397644ca7a7f59a010
AngelD <angelduarteing@gmail.com>
2023-01-30 11:16:57 -0500

    metodos

---
+++ Metodos/meotods dict.py
@@ -0,0 +1,28 @@
+diccionario = {
+    "nombre"    : "Angel",
+    "apellido"  : "Duarte",
+    "subs"      : 10000,
+    "cabello"   : "dorado"
+}
+
+# muestra todos los items que contiene el diccionario (dict)
+claves = diccionario.keys() 
+print(claves)
+
+# busca el item en el diccionario, si no lo encuentra sale error
+buscar = diccionario["nombre"] 
+# get : es necesario para que no salga error cuando no encuentra el item en el diccionario y da como resultado "none"
+buscar = diccionario.get("nombre") 
+print(buscar)
+
+# pop : elimina un item del diccionario
+eliminar = diccionario.pop("apellido")
+print(diccionario)
+
+# items : itera cada item con su significado en un vector
+iterable = diccionario.items()
+print(iterable)
+
+# clear : elimina todos los item del diccionario
+eliminar_todo = diccionario.clear()
+print(diccionario)
---
+++ Metodos/metodos lista.py
@@ -0,0 +1,41 @@
+# list : funcion para crear una lista
+lista = list(["hola", "angel", "eduardo", 34, 1.78])
+print(lista)
+
+# len : funcion que deveulve la cantidad de elementos de una lista 
+print(len(lista))
+
+# append : metodo para agregar un elemento a una lista, primero lo agrega y luego lo llama (dos lineas)
+lista.append("soltero")
+print(lista) 
+
+# insert : metodo para agregar un elemento a una lista en la posición dada
+lista.insert(1,"mi bello")
+print(lista) 
+
+# extend : metodo para agregar varios elementos a una lista 
+lista.extend(["soy rico","colombiano"])
+print(lista) 
+
+# pop : metodo para eliminar un elemento de una lista en la posición dada
+lista.pop(2) # elimina el tercer elemento
+lista.pop(-1) # (-1) elimina el ultimo elemento, (-2) elimina el penultimo, etc.
+print(lista) 
+
+# remove : metodo para eliminar un elemento de una lista segun el dato dado
+lista.remove(1.78) # si no esta el dato exacto no lo elimina
+print(lista) 
+
+# sort : metodo que ordena los elementos de forma ascendente, (enteros) 
+lista_2 = list([8,2,6,9,4,3,1,5,0])
+lista_2.sort()
+print(lista_2)
+
+lista_2.sort(reverse=True) # reverse=True ; cambiar el orden a descendente
+print(lista_2)
+
+# reverse : cambia el orden de una cadena de atras hacia delante
+lista.reverse()
+lista_2.reverse()
+print(lista)
+print(lista_2)
