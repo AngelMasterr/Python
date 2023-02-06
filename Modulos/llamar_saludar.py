80a427c4575f72290febd66993d1e221da21537d
AngelD <angelduarteing@gmail.com>
2023-02-06 12:05:12 -0500

    actual

---
+++ Modulos/llamar_saludar.py
@@ -0,0 +1,8 @@
+# Import: importa el modulo deseado
+
+import modulo_saludar
+
+# La funcion "saludar" que esta dentro del medulo "modulo_saludar" ahora se convierte en un metodo
+
+saludo = modulo_saludar.saludar("angel")
+print(saludo)
---
+++ Modulos/modulo_saludar.py
@@ -0,0 +1,7 @@
+# Un modulo es un archivo de python, cualquier archivon que termine en .py es un modulo
+# Los modulos se usan para crear funciones especificas para luego llamarlas desde otro archivo
+# practicamente todo lo que hacemos es un modulo, pero no sirve para organizar mejor el trabajo
+
+def saludar(name):
+    return(f"hola {name} espero que hayas tenido un día maravilloso")
+    
