487732d77514de92fbaf107d20948017f3b6c61a
AngelD <angelduarteing@gmail.com>
2023-03-07 14:44:54 -0500

    mas

--- 8_Ejercicios_HR_problems/tripletas_ratio.py
+++ 8_Ejercicios_HR_problems/tripletas_ratio.py
@@ -5,39 +5,36 @@ arr = [1,4,16,64] -- r = 2
 there are [1,4,16] and [4,16,64] = 2 tripets with geometric progresion is 4
 """
 import math
-from itertools import combinations
 from collections import Counter
 
 def countTriplets(arr, r):  
+    arr = sorted(arr)
     list_count = Counter(arr) 
-    print(list_count)   
-    if len(list_count) >= 3:
-        list_com = list(combinations(list_count,3))    
-        triple = []          
-        for i in list_com:
-            if i[2]/i[1] == r and i[1]/i[0] ==r:
-                triple.append(i) 
+    max_count = max(list_count)
+    if r > 1:
+        print(list_count) 
+        print(max_count) 
+        factor = 0
+        for j in list_count: 
+            if j*r in list_count and j*r*r in list_count:
+                factor += max(list_count[j], list_count[j*r], list_count[j*r*r])
+            elif j*r*r > max_count:
+                break
+        return(factor)        
     else:
         combi = 0
         for i in list_count:
             combi += math.comb(list_count[i],3)
-        return(combi)
-                       
-    value_max = 0
-    for i in triple:
-        value_max += max(list_count[x] for x in i)        
-    print(triple)
-    return(value_max)
+        return(combi)  
 
-with open("Archivos//8_tripletas_prueba4.txt","r") as prueba:
+with open("Archivos//8_tripletas_prueba2.txt","r") as prueba:
     list_arr = prueba.readline()
     arr = list(map(int, list_arr.strip().split()))
-    r = 1
+    r = 2
     print(countTriplets(arr, r))
+
 
 arr = [1, 3, 9, 9, 27, 81]
 r = 3
 #print(countTriplets(arr, r))
 
-# 166661666700000 
-# 166661666700000
