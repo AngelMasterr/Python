9625ebd2bac63089cb34b90c009aa8cce390248b
AngelD <angelduarteing@gmail.com>
2023-03-13 12:49:27 -0500

    mas

---
+++ 10_Ejercicios_HR_datetime/diff_fechas_datetime.py
@@ -0,0 +1,24 @@
+# datetime: this module helps us to find the difference betwen two or more dates, 
+
+from datetime import datetime
+
+fecha_str1 = "Fri 01 May 2015 13:54:36 -0000"
+fecha_str2 = "Sat 02 May 2015 15:43:21 -0000"
+
+format_str = "%a %d %b %Y %H:%M:%S %z" 
+# %a: corresponds to the name of the day
+# %d: corresponds to the number of the day
+# %b: corresponds to the name of the month
+# %Y: corresponds to the number of the year
+# %H:%M:%S corresponds to the hour, minuts and seconds of the moment
+# %z: correspons to the time zone
+
+# convert the dates to the format of datetime (format_str)
+
+fecha_1 = datetime.strptime(fecha_str1, format_str)
+fecha_2 = datetime.strptime(fecha_str2, format_str)
+
+difference = fecha_1 - fecha_2
+diff_seconds = difference.total_seconds()
+
+print(abs(diff_seconds))
--- Metodos/meotods_set.py
+++ Metodos/meotods_set.py
@@ -29,6 +29,19 @@ myset.remove(13)
 print(myset)
 # {'a', 1, 'c', 'b', 4, 5, 6, 7, 8, 2, (5, 4), 3}
 
+a = {2, 4, 5, 9}
+b = {2, 4, 11, 12}
+a.union(b) # Values which exist in a or b
+# {2, 4, 5, 9, 11, 12}
+a.intersection(b) # Values which exist in a and b
+# {2, 4}
+a.difference(b) # Values which exist in a but not in b
+# {9, 5}
+
+# ^: diferencia simétrica, y se puede entender como la unión de los conjuntos que no tienen elementos en común.
+a ^ b 
+# {5, 9, 11, 12}
+
 # add(elemento): Agrega un elemento al conjunto.
 
 # remove(elemento): Elimina un elemento del conjunto. Si el elemento no se encuentra, se lanza un error KeyError.
@@ -41,3 +54,5 @@ print(myset)
 
 # difference(set2): Devuelve un nuevo conjunto que contiene los elementos del conjunto actual que no están en el conjunto set2.
 
+# set_a ^ set_b: ^: diferencia simétrica la unión de los conjuntos que no tienen elementos en común. El resultado será 
+# un nuevo conjunto que contiene los elementos que están en uno de los conjuntos, pero no en ambos:
