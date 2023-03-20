5f30d824ef6f75b997848efba38b1e5d9414615a
AngelD <angelduarteing@gmail.com>
2023-03-20 12:53:52 -0500

    as

--- probando.py
+++ probando.py
@@ -1,2 +1,10 @@
 string = "angelduarte"
 print(string[::-1])
+
+ransomNote = "fihjjjjei"; magazine = "hjibagacbhadfaefdjaeaebgi"
+
+ransomNote = ransomNote.replace("j","z")
+print(ransomNote)
+
+"fihjjjjei"
+"hjibagacbhadfaefdjaeaebgi"
