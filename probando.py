daaf954746c35904310858f9c44abb5bb1c55612
AngelD <angelduarteing@gmail.com>
2023-03-24 16:11:56 -0500

    mas

--- probando.py
+++ probando.py
@@ -1,9 +1,11 @@
-try:
-    print("sfasfa")
-except:
-    print("sfaasa")
-finally:
-    print("asdasda")
-    y = [2, 5j, 6]
-    y.sort()
-    print(y)
+s = "abcadeaj"
+
+list_let=[]
+for i in range(len(s)-1):
+    if s[i] in s[i+1:]:
+        new = s[::-1] 
+        print(new)   
+        ind = new.find(s[i])
+        list_let.append(len(new)-2-ind-i)
+print(max(list_let))
+    
