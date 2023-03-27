218da211980e52a62ea3570e23923707c5218563
AngelD <angelduarteing@gmail.com>
2023-03-26 21:21:01 -0500

    mmm

---
+++ 1_aVarios_important/binary_tree_nodes.py
@@ -0,0 +1,43 @@
+# Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+"""For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar."""
+
+"""Example7
+root1 = [3,5,1,6,2,9,8,null,null,7,4], 
+root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+       3			 3
+     /   \		   /   \
+    5     1		  5     1
+   / \   /  \	 / \   / \
+  6   2 9   8	6   7 4   2
+     / \		         / \
+    7   4		        9   8
+leaf root1 = [6,7,4,9,8]
+leaf root2 = [6,7,4,9,8]
+output = True		 
+"""
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(TreeNode):
+    def leafSimilar(self, root1, root2)-> bool:
+        super().__init__()   
+        def dfs(node):
+            if node:
+                if not node.left and not node.right:
+                    yield node.val
+                yield from dfs(node.left)
+                yield from dfs(node.right)
+
+        return list(dfs(root1)) == list(dfs(root2))
+    
+root1 = [3,5,1,6,2,9,8,None,None,7,4]
+root2 = [3,5,1,6,7,4,2,None,None,None,None,None,None,9,8]
+
+leaf = Solution(TreeNode)
+print(leaf.leafSimilar(root1, root2))
