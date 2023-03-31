50ff80991bdb83ab7e24f343403da62428497403
AngelD <angelduarteing@gmail.com>
2023-03-30 20:24:58 -0500

    mas

--- 1_Ejercicios/ejercicio_1.4.py
+++ 1_Ejercicios/ejercicio_1.4.py
@@ -3,3 +3,12 @@ k = int(input())
 rooms = list(map(int, input().split()))
 room_cap = [x for x,y in (Counter(rooms)).items() if y == 1]
 print(*room_cap)
+
+t = int(input())
+for i in range(t):
+    a = int(input())
+    set_a = set(map(int, input().split()))
+    b = int(input())
+    set_b = set(map(int, input().split()))
+    print(set_a.issubset(set_b))
+     
