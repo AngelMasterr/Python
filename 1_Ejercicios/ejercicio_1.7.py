b186d3a1c04c5e33bdb1ff65b341ac734054abc6
AngelMasterr <angelduarteprog@gmail.com>
2024-01-11 19:53:55 -0500

    ciclo for y lista

--- 1_Ejercicios/ejercicio_1.7.py
+++ 1_Ejercicios/ejercicio_1.7.py
@@ -1,6 +1,6 @@
 # repasando conceptos basicos en python - CICLOS
 
-
+# MOstrar mensaje si tiene permiso para conducir
 def verificar_si_puede_conducir(edad):
    if edad >= 18:
       print("Usted puede conducir perrito")
@@ -11,3 +11,13 @@ edades = [18, 15, 36, 25, 12]
 
 for edad in edades:
    verificar_si_puede_conducir(edad)
+
+# la misma funcion de arriba pero optiomizada
+def verificar_si_puede_conducir_2(edades):
+   for edad in edades:
+      if edad >= 18:
+         print("Usted puede conducir perrito")
+      else:
+         print("NO puede conducir, eres menor de edad")
+      
+verificar_si_puede_conducir_2(edades)
